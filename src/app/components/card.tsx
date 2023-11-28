af5b8f5f49a8d910d127ff74a2d61e3b226b6ecc
Rotola Akinsowon <rotolaakinsowon@yahoo.com>
2023-11-28 17:35:21 +0100

    initial commit

---
+++ src/app/components/card.tsx
@@ -0,0 +1,12 @@
+
+type CardProps = {
+
+}
+
+export default function Card(props: CardProps) {
+    return (
+        <div className="absolute m-10 p-5 min-h-[50%] max-h-fit min-w-[50%] bg-amber-50 drop-shadow-xl rounded-xl">
+
+        </div>
+    )
+}
